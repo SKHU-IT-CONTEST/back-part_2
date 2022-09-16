887c53febaa295d3df26e38585aeb64777855ef1
Hwang JaeHyeon <ten02130735@gmail.com>
2022-09-16 21:32:53 +0900

    완성(ver. 0.1.0)

--- test_1.js
+++ test_1.js
@@ -1,55 +1,51 @@
-// var request = require('request');
-// var fs = require('fs')
-// var url = 'http://apis.data.go.kr/3160000/guroPointFocInfoSvc/getGuro10PointFocInfoSvc';
-// var queryParams = '?' + encodeURIComponent('serviceKey') + '=XXsK%2F1XwVTPaVFfkrpoBQapqSlNiziqMMJJRcS549BH3B2gH1ph4mkRwBJgDbI20uZDnt9SiLbsVlFT5%2FAHCBQ%3D%3D'; /* Service Key*/
-// queryParams += '&' + encodeURIComponent('returnType') + '=' + encodeURIComponent('xml'); /* */
-// queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10'); /* */
-// queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /* */
+// 필요한 항목 : 체감온도 데이터 + 앞으로의 3시간의 데이터
+
+/* NodeJs 12 샘플 코드 */
+
+
+var request = require('request');
+var convert = require('xml-js');
+var fs = require('fs');
+
+var url = 'http://apis.data.go.kr/3160000/guroPointFocInfoSvc/getGuro10PointFocInfoSvc';
+var queryParams = '?' + encodeURIComponent('serviceKey') + '=XXsK%2F1XwVTPaVFfkrpoBQapqSlNiziqMMJJRcS549BH3B2gH1ph4mkRwBJgDbI20uZDnt9SiLbsVlFT5%2FAHCBQ%3D%3D'; /* Service Key*/
+queryParams += '&' + encodeURIComponent('returnType') + '=' + encodeURIComponent('xml'); /* */
+queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10'); /* */
+queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /* */
+
+request.get(url + queryParams, (err, res, body) => {
+    if(err) {
+        console.log(`err => ${err}`)
+    }
+    else {
+        if(res.statusCode == 200) {
+            var result = body
+            console.log(`body data => ${result}`)
+            var xmlToJson = convert.xml2json(result, {compact: true, spaces: 4});
+            console.log(`xml to json => ${xmlToJson}`)
+        }
+        fs.writeFileSync('example.json', xmlToJson);
+    }
+})
 
 
 // request({
 //     url: url + queryParams,
 //     method: 'GET'
 // }, function (error, response, body) {
+//     const a = JSON.stringify(response.statusCode);
+//     const b = JSON.stringify(response.headers);
+//     const c = JSON.stringify(body);
 //     console.log('Status', response.statusCode);
 //     console.log('Headers', JSON.stringify(response.headers));
 //     console.log('Reponse received', body);
-    
-//     // console.log(JSON.stringify(headers))
-//     // console.log(JSON.stringify(bodies))
+//     fs.writeFileSync('example1.json', a + b + c);
+//     // var json = convert.xml2json(body, {compact: true, spaces: 4});
+//     // var data = JSON.parse(json).response.body.items.item;
+//     // var localCode = data['localCode']._text;
+//     // var humi = data['humi']._text;
+//     // var rain = data['rain']._text;
+//     // console.log('localcode: ', localCode);
+//     // console.log('humi: ', humi);
+//     // console.log('rain: ', rain);
 // });
-
-// 상수값 정의
-const ServiceKey = 'XXsK%2F1XwVTPaVFfkrpoBQapqSlNiziqMMJJRcS549BH3B2gH1ph4mkRwBJgDbI20uZDnt9SiLbsVlFT5%2FAHCBQ%3D%3D'
-const numOfRows = 10;
-const pageNo = 1;
-
-// module
-let request = require('request');
-
-var url = 'http://apis.data.go.kr/3160000/guroPointFocInfoSvc/getGuro10PointFocInfoSvc'; 
-var queryParams = '?' + encodeURIComponent('serviceKey') + '=' + encodeURIComponent(ServiceKey); /* Service Key */
-queryParams += '&' + encodeURIComponent('returnType') + '=' + encodeURIComponent('xml'); /* */
-queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent(numOfRows); /* */
-queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent(pageNo); /* */
-
-let options = {
-    method: 'GET',
-    url: url + queryParams
-};
-
-request(options, function(error, response, body) {
-    if(error) {
-        throw new Error(error);
-    }
-    let info = JSON.parse(body);
-
-    // test block
-    for(i in info['response']['body']['items']['item']) {
-        console.log('발표일시:' + info['response']['body']['items']['item'][i]['baseDate'])
-    }
-
-    console.log('Status', response.statusCode);
-    console.log('Headers', JSON.stringify(response.headers));
-    console.log('Reponse received', body);
-})
