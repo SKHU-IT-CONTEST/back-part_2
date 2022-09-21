a024309f5a6eae27c05397c0e0c6bf82a092fc0e
Hwang JaeHyeon <ten02130735@gmail.com>
2022-09-21 23:13:25 +0900

    올바른 json 데이터가 되도록 문자열 조작처리코드 추가 완료(ver. 0.3.2)

--- main.js
+++ main.js
@@ -71,7 +71,15 @@
 var request = require('request');
 var convert = require('xml-js');
 var fs = require('fs');
-const { json } = require('express');
+
+const date = new Date();
+
+var year = date.getFullYear();
+var month = ('0' + (date.getMonth() + 1)).replace(-2);
+var hours = date.getHours();
+var nowDetailDate = "";
+nowDetailDate = year + "" + month + "" + hours + "" + "00";
+console.log(nowDetailDate);
 var url = 'http://apis.data.go.kr/3160000/guroPointFocInfoSvc/getGuro10PointFocInfoSvc';
 var queryParams = '?' + encodeURIComponent('serviceKey') + '=XXsK%2F1XwVTPaVFfkrpoBQapqSlNiziqMMJJRcS549BH3B2gH1ph4mkRwBJgDbI20uZDnt9SiLbsVlFT5%2FAHCBQ%3D%3D'; /* Service Key*/
 queryParams += '&' + encodeURIComponent('returnType') + '=' + encodeURIComponent('xml'); /* */
@@ -85,25 +93,26 @@ request.get(url + queryParams, (err, res, body) => {
     }
     else {
         if(res.statusCode == 200) {
-
             
             var result = body
             console.log(`body data => ${result}`) // 사전에 전체 데이터를 출력
             var selectedData = JSON.parse(convert.xml2json(result, {compact: true, spaces: 4}));
             fs.writeFileSync('./back-part_2/alldata.json', JSON.stringify(selectedData));
-            var a = "";
+            var a = "[";
             var tmpData = selectedData.response.body.items.item;
             for(let i=0; i<tmpData.length; i++) {
                 if((tmpData[i]['localCode']._text === "GURO_F08")) { // 회대 지역의 날씨 데이터가 맞다면
 
-                    a = a + JSON.stringify(tmpData[i]); // 회대 지역(GURO_F08)의 내용만 추출하여 json 파일에 저장함
+                    a += JSON.stringify(tmpData[i]); a += ",";  // 회대 지역(GURO_F08)의 내용만 추출하여 json 파일에 저장함
                     
                 }
             }
-            fs.writeFileSync("./back-part_2/modified.json", JSON.stringify(a)); // 저장되는 데이터는 1시간 간격으로 업데이트 처리됨
+            a = a.substring(0, a.length -1); // json 구조의 규칙에 맞게끔 마지막 데이터의 ',' 문자 제거 처리
+            a += "]"; // 데이터 추가 전의 '[' 기호에 알맞게 대응되도록 ']' 기호 추가
+            fs.writeFileSync("./back-part_2/modified.json", a); // 저장되는 데이터는 1시간 간격으로 업데이트 처리됨
         }
         
         
-        // fs.writeFileSync('/Users/hwangjaehyeon/my-json-server/db.json', xmlToJson); // 개인용 임시서버 활용을 위한 저장용 코드
+        
     }
 })
